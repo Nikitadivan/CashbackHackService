1cbdeef3c6661e65db64acb617d70d75530ae77a
Nikitadivan <sarapul-mebel@mail.ru>
2022-04-27 23:27:50 +0400

    commit for junit4 branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,45 +1,36 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import junit.framework.TestCase;
 
-import static org.testng.Assert.*;
+public class CashbackHackServiceTest extends TestCase {
 
-public class CashbackHackServiceTest {
-
-    @org.testng.annotations.Test
     public void testRemain1() {
-        CashbackHackService service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService ();
         int amount = 2500;
         int expected = 500;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
-
+        assertEquals(expected,actual);
     }
-    @org.testng.annotations.Test
     public void testRemain2() {
-        CashbackHackService service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService ();
         int amount = 999;
         int expected = 1;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
-
+        assertEquals(expected,actual);
     }
-    @org.testng.annotations.Test
     public void testRemain3() {
-        CashbackHackService service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService ();
         int amount = 1000;
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
-
+        assertEquals(expected,actual);
     }
-    @org.testng.annotations.Test
     public void testRemain4() {
-        CashbackHackService service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService ();
         int amount = 1001;
         int expected = 999;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
-
+        assertEquals(expected,actual);
     }
+
 }
