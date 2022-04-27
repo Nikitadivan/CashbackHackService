1e52266d2f7dfcd7d21c85be6f62a8b67333f4ba
Nikitadivan <sarapul-mebel@mail.ru>
2022-04-27 22:37:01 +0400

    after modification testRemain 3 with amount = 1000

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -28,7 +28,7 @@ public class CashbackHackServiceTest {
     public void testRemain3() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
         assertEquals(actual,expected);
 
